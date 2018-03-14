f3f5cb773078ff92e367a98a3cadd71f55f52126
aRandomDecoy <jborow5@uic.edu>
2018-03-14 01:13:50 -0500

    Stableized Camera, added a UI Lap Counter, added a finishLine

---
+++ Baja/Assets/Scripts/StableCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StableCamera : MonoBehaviour {
+
+	public GameObject Car;
+	public float CarXAxis;
+	public float CarYAxis;
+	public float CarZAxis;
+
+
+	// Update is called once per frame
+	void Update () {
+		CarXAxis = Car.transform.eulerAngles.x;
+		CarYAxis = Car.transform.eulerAngles.y;
+		CarZAxis = Car.transform.eulerAngles.z;
+
+		transform.eulerAngles = new Vector3 (CarXAxis - CarXAxis, CarYAxis, CarZAxis - CarZAxis);
+
+		
+	}
+}
