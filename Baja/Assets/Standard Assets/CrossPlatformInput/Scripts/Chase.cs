f3f7f8ec983757dbd1f94bfcb080d0af86b7021e
Iqra <imemon3>
2018-03-20 19:24:57 -0500

    2nd AI Chase FSM

---
+++ Baja/Assets/Standard Assets/CrossPlatformInput/Scripts/Chase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+    Vector3 pos;
+    Rigidbody rb;
+    Transform t;
+
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            if (direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 5.0f);
+            }
+     
+    }
+}
