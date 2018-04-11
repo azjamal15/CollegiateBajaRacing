ad0e00189f82541d6f568ab7cf8dbad092d183e6
Azam Jamal <ajamal6@uic.edu>
2018-04-11 11:33:01 -0500

    Added Menu Scene and Writing

---
+++ Baja/Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ButtonOptions : MonoBehaviour {
+	public void PlayGame () { 
+		SceneManager.LoadScene(1);
+	}
+
+	public void Menu() { 
+		SceneManager.LoadScene (0); 
+	}
+
+	public void Info1 () { 
+		SceneManager.LoadScene (2);
+	}
+
+	public void Info2 () { 
+		SceneManager.LoadScene (3); 
+	}
+
+
+}
