09bc972ab4ef65e4fefe701111480a50c315bbd6
Azam Jamal <ajamal6@uic.edu>
2018-04-17 15:58:47 -0500

    Lastest Changes, Team :)

---
+++ Baja/Assets/Scripts/CompleteLap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CompleteLap : MonoBehaviour {
+
+	public GameObject midwayTrigger;
+	public GameObject lapCompleteTrigger; 
+
+	public GameObject minDisplay; 
+	public GameObject secDisplay; 
+	public GameObject milliDisplay; 
+
+	public GameObject lapBox; 
+
+	void OnTriggerEnter() { 
+		if (LapManager.SecondCount <= 9) { 
+			secDisplay.GetComponent<Text> ().text = "0" + LapManager.SecondCount + ".";
+		} 
+		else { 
+			secDisplay.GetComponent<Text> ().text = "" + LapManager.SecondCount + "."; 
+		}
+
+		if (LapManager.MinuteCount <= 9) { 
+			minDisplay.GetComponent<Text> ().text = "0" + LapManager.MinuteCount + ".";
+		} 
+		else { 
+			minDisplay.GetComponent<Text> ().text = "" + LapManager.MinuteCount + "."; 
+		}
+
+
+	} 
+}
---
+++ Baja/Assets/Scripts/MidwayTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidwayTrigger : MonoBehaviour {
+
+	public GameObject midwayTrigger; 
+	public GameObject lapCompleteTrigger; 
+
+	void OnTriggerEnter() { 
+		lapCompleteTrigger.SetActive (true);
+		midwayTrigger.SetActive (false);
+	}
+}
