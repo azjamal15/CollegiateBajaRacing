459a36148e1977e9c371713f364b0c4aa70d165c
Azam Jamal <ajamal6@uic.edu>
2018-04-17 04:11:43 -0500

    Added countdown for beginning of race

---
+++ Baja/Assets/Scripts/CarControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour {
+
+	public GameObject control;
+
+	// Use this for initialization
+	void Start () {
+		control.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>().enabled = true;
+		
+	}
+	
+
+}
+
---
+++ Baja/Assets/Scripts/CountDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CountDown : MonoBehaviour {
+
+	public GameObject countDown; 
+	public AudioSource go;     //varible for 'GoAudio.wav'
+	public AudioSource ready;  //variable for 'GetReady.wav' 
+	public GameObject timer;   //timer for lap 
+	public GameObject control; //for car controller 
+
+	void Start () {
+		StartCoroutine (CountStart ());
+
+	}
+	
+	/* Starts the counter */
+	IEnumerator CountStart() { 
+		yield return new WaitForSeconds (0.5f); 
+		countDown.GetComponent<Text> ().text = "3";
+		ready.Play ();
+		countDown.SetActive (true); 
+
+		yield return new WaitForSeconds (1);
+		countDown.SetActive (false); 
+		countDown.GetComponent<Text> ().text = "2";
+		ready.Play ();
+		countDown.SetActive (true);
+
+		yield return new WaitForSeconds (1);
+		countDown.SetActive (false);
+		countDown.GetComponent<Text> ().text = "1";
+		ready.Play ();
+		countDown.SetActive (true);
+
+		yield return new WaitForSeconds (1);
+		countDown.SetActive (false);
+		countDown.GetComponent<Text> ().text = "GO!"; 
+		go.Play ();
+
+		timer.SetActive (true);
+		control.SetActive (true);
+	} 
+}
